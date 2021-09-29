ad660ec8db111087cf766add9901e57cda8a3a5d
KillerInk <KillerInk@email.de>
2021-09-29 13:09:29 +0200

    handel weight limit, fix RateLimitType parser error

--- src/main/java/com/binance/api/client/impl/BinanceApiServiceGenerator.java
+++ src/main/java/com/binance/api/client/impl/BinanceApiServiceGenerator.java
@@ -3,12 +3,15 @@ package com.binance.api.client.impl;
 import com.binance.api.client.api.BinanceApiError;
 import com.binance.api.client.api.BinanceApiService;
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.binance.api.client.exception.BinanceApiException;
 import com.binance.api.client.security.AuthenticationInterceptor;
 import okhttp3.Dispatcher;
+import okhttp3.Headers;
 import okhttp3.OkHttpClient;
 import okhttp3.ResponseBody;
+import okhttp3.internal.http2.Header;
 import retrofit2.Call;
 import retrofit2.Converter;
 import retrofit2.Response;
@@ -17,6 +20,8 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
 
 import java.io.IOException;
 import java.lang.annotation.Annotation;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -60,13 +65,54 @@ public class BinanceApiServiceGenerator {
         return retrofit.create(serviceClass);
     }
 
+    public static int limit;
+    private final static int LimitPerMin = 1200;
+    private static long limitStartTime;
+    private static long limitEndTime;
+
+    private static void handelWeightLimit()
+    {
+        if (limit >= LimitPerMin) {
+            Log.d("executeSync", "limit reached wait 5 sec");
+            try {
+                Thread.sleep(limitEndTime - System.currentTimeMillis());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void  parseWeightLimit(Response response)
+    {
+        Headers headerList = response.headers();
+        try
+        {
+            List<String> l = headerList.values("x-mbx-used-weight");
+            if (l != null && l.size() >= 1) {
+                int li = Integer.parseInt(l.get(0));
+                if (li < limit)
+                {
+                    limitStartTime = System.currentTimeMillis();
+                    limitEndTime = limitStartTime + 60*1000;
+                }
+                limit = li;
+                Log.d("executeSync", "limit: " + limit);
+            }
+        }
+        catch (NullPointerException nullPointerException)
+        {
+        }
+    }
+
     /**
      * Execute a REST call and block until the response is received.
      */
     public static <T> T executeSync(Call<T> call) {
         try {
+            handelWeightLimit();
             Response<T> response = call.execute();
             if (response.isSuccessful()) {
+                parseWeightLimit(response);
                 return response.body();
             } else {
                 BinanceApiError apiError = getBinanceApiError(response);
@@ -76,6 +122,7 @@ public class BinanceApiServiceGenerator {
             throw new BinanceApiException(e);
         }
     }
+
 
     /**
      * Extracts and converts the response error body into an object.
